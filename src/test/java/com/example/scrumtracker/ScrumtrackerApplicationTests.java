56929d5293de3aa82823c110f51cb6f4662a93b7
erdemcemozer <erdemcemozer@hotmail.com>
2021-02-21 12:56:06 +0300

    scrumtracker starter

---
+++ src/test/java/com/example/scrumtracker/ScrumtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrumtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
