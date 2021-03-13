465a17a88c08c865ef69d896485869fcb2f800b6
erdemcemozer <erdemcemozer@hotmail.com>
2021-03-14 00:09:20 +0300

    Added web controller

---
+++ src/main/java/com/example/scrumtracker/controller/MainWebController.java
@@ -0,0 +1,18 @@
+package com.example.scrumtracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author erdemcemozer
+ */
+
+@Controller
+public class MainWebController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homepage(){
+        return "signinpage"; // will look later
+    }
+}
