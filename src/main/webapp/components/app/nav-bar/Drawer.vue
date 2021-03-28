178f31dc5086074b18914903ff396368e118a9f8
Sadettin Ayyıldız <53340224+SadettinAyyildiz@users.noreply.github.com>
2021-03-28 13:55:25 +0300

    v1

---
+++ src/main/webapp/components/app/nav-bar/Drawer.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-navigation-drawer v-model="drawer"
+                         app
+                         fixed
+                         floating
+                         color="light"
+                         :mini-variant="miniVariant"
+                         mini-variant-width="64"
+                         width="280"
+                         class="elevation-12"
+                         :class="[{'rounded-r-xl': $vuetify.breakpoint.lgAndUp}]">
+
+<!--        :expand-on-hover="expandOnHover"-->
+        <template #prepend>
+            <app-nav-bar-top-side/>
+        </template>
+
+        <app-nav-bar-nav-menu :miniVariantStatus="miniVariant"/>
+
+        <template #append>
+            <app-nav-bar-bottom-side :miniVariantStatus="miniVariant"/>
+        </template>
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: "NavBarDrawer",
+    data () {
+        return {
+            drawer: true,
+        }
+    },
+    computed:{
+
+        miniVariant(){
+            return this.$store.state.miniVariant
+        },
+
+        expandOnHover(){
+            return this.miniVariant === true
+        }
+    }
+}
+</script>
---
+++ src/main/webapp/nuxt.config.js
@@ -0,0 +1,75 @@
+import colors from 'vuetify/es5/util/colors'
+
+import i18n from './plugins/i18n'
+
+export default {
+    head: {
+        titleTemplate: '%s - scrumtracker',
+        title: 'scrumtracker',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
+    },
+
+    css: [],
+
+    plugins: [],
+
+    components: {
+        dirs: [
+            '@/components',
+            '@/components/pages',
+            {
+                path: '@/components/app',
+                prefix: 'app'
+            }
+        ]
+    },
+
+    buildModules: [
+        '@nuxtjs/vuetify',
+    ],
+
+    modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        'nuxt-i18n',
+        '@nuxtjs/universal-storage'
+    ],
+
+    axios: {},
+
+    i18n,
+
+    vuetify: {
+        customVariables: ['~/assets/scss/variables.scss'],
+        theme: {
+            dark: false,
+            options: {
+                customProperties: true,
+            },
+            themes: {
+                dark: {
+                    light: '#ecf2f6',
+                    primary: colors.blue.darken2,
+                    accent: colors.grey.darken3,
+                    secondary: colors.amber.darken3,
+                    info: colors.teal.lighten1,
+                    warning: colors.amber.base,
+                    error: colors.deepOrange.accent4,
+                    success: colors.green.accent3
+                },
+                light: {
+                    light: '#ecf2f6',
+                },
+            }
+        }
+    },
+
+    build: {}
+}
