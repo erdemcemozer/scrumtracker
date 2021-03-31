8a5d2e1f1133612cf6771b70ba08d835f592a0e8
mkurfeyiz <m.kurfeyiz98@gmail.com>
2021-03-31 22:26:06 +0300

    User-crud first implementation, added create/update/delete methods

--- src/main/java/com/example/scrumtracker/service/UserServiceImpl.java
+++ src/main/java/com/example/scrumtracker/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.scrumtracker.service;
 import com.example.scrumtracker.model.Users;
 import com.example.scrumtracker.repository.UserMongoDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author erdemcemozer
@@ -18,5 +20,31 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void createUser(Users user) {
 		userMongoDao.save(user);
+	}
+
+	@Override
+	public void updateUser(Users user, String newUsername, String newPassword) {
+
+		if (userMongoDao.existsById(user.getId())) {
+			// Setting new username and password
+			user.setUsername(newUsername);
+			user.setPassword(newPassword);
+
+			userMongoDao.save(user);
+		} else {
+			System.out.println("Nerede lan bu herif.");
+		}
+
+	}
+
+	@Override
+	public void deleteUser(Users user, String id) {
+
+		if (userMongoDao.existsById(user.getId())) {
+			// Deleting user by id
+			userMongoDao.deleteById(id);
+		} else {
+			System.out.println("Bu eleman burada yok.");
+		}
 	}
 }
