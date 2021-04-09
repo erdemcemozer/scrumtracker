984a40851b8729ba1e53a52060a2775692f7cfe5
erdem özer <erdemcemozer@hotmail.com>
2021-04-10 00:50:49 +0300

    Dashboard crud starter

---
+++ src/main/java/com/example/scrumtracker/controller/DashboardController.java
@@ -0,0 +1,18 @@
+package com.example.scrumtracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author erdemcemozer
+ */
+
+@RequestMapping("issues")
+@RestController
+public class DashboardController {
+
+	/*
+	 * TODO dashboard will have sprint name, description for sprint, total issues,
+	 * TODO how many in which state and analytics for issue priorities and types.
+	 */
+}
